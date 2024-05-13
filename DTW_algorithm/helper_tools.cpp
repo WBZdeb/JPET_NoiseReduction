f957208b8d9dc112cee390ea84157add89a0dd98
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2024-05-13 11:21:02 +0200

    Add function that writes reports to file.

--- DTW_algorithm/helper_tools.cpp
+++ DTW_algorithm/helper_tools.cpp
@@ -1,5 +1,15 @@
 #include <TVector3.h>
+
+#include <TROOT.h>
+#ifdef R__HAS_VDT
+#undef R__HAS_VDT
+#endif
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RDF/RInterface.hxx" /// for RNode
+
 const double kLightVelocity_cm_ps = 0.0299792458;
+
+using RDF = ROOT::RDataFrame;
 
 TVector3 calculateAnnihilationPoint(const TVector3& hitA, const TVector3& hitB, double tof)
 {
@@ -11,4 +21,14 @@ TVector3 calculateAnnihilationPoint(const TVector3& hitA, const TVector3& hitB, 
                              middleOfLOR.Y() + shift * versorOnLOR.Y(),
                              middleOfLOR.Z() + shift * versorOnLOR.Z());
   return annihilationPoint;
+}
+
+void saveReportToFile(const std::string& outFileName, ROOT::RDF::RCutFlowReport& report)
+{
+  std::ofstream file(outFileName.c_str());
+  file << std::left << std::setw(30) << "Cut condition" << std::setw(30) << "All" << std::setw(20) << "Passed" << std::setw(15) << "Percentege" << std::endl;
+  for (auto  cutInfo : report) {
+    file << std::left << std::setw(30) << cutInfo.GetName() << std::setw(30) << cutInfo.GetAll() << std::setw(20) << cutInfo.GetPass() << std::setw(15)
+         << cutInfo.GetEff() << " %" << std::endl;
+  }
 }
